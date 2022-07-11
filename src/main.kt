7fe8c4c5531ffa3279698c77c0ab7553e5525cdc
Filipe Soares Cancio <filipe.cancio@neon.com.br>
2022-07-11 15:14:27 -0300

    solucao #1

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,16 @@
+
 fun main() {
-    print("Hello World")
+    val a = "xyaabbbccccdefww"
+    val b = "xxxxyyyyabklmopq"
+    print(longest(a,b))
 }
+
+fun longest(str1:String ,str2:String):String {
+    //join the strings
+    val joinStr = str1+str2
+    //remove duplicates and sort characters
+    val sortStr = joinStr.toSortedSet()
+    //rejoins all chars to a string
+    return sortStr.joinToString("")
+}
+
